8b25e5a55fb551ffaa40d53d199b69e28a21380b
Marcellin Le Martien <mrwego@excilys.com>
2015-03-19 15:24:03 +0100

    Mise en place du Web UI

---
+++ src/test/java/com/excilys/computerdb/dao/CompanyDAOTest.java
@@ -0,0 +1,29 @@
+package test.java.com.excilys.computerdb.dao;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class CompanyDAOTest {
+	static final String PROPERTIES_FILE = "dao.properties";
+	InputStream inputStreamMock;
+	ClassLoader classLoader;
+	Properties propertiesMock;
+
+	@Before
+	public void setUp() throws Exception {
+		propertiesMock = Mockito.mock(Properties.class);
+		//Mockito.when(propertiesMock.load(inputStreamMock)).
+	}
+
+	@Test
+	public final void testCompanyDAO() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/test/java/com/excilys/computerdb/dao/DAOFactoryTest.java
+++ src/test/java/com/excilys/computerdb/dao/DAOFactoryTest.java
@@ -2,33 +2,17 @@ package test.java.com.excilys.computerdb.dao;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.InputStream;
-import java.util.Properties;
-
 import main.java.com.excilys.computerdb.dao.DAOFactory;
-import main.java.com.excilys.computerdb.exception.*;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class DAOFactoryTest {
-	
-	private static final String PROPERTIES_FILE = "dao.properties";
-	private static final String PROPERTY_URL = "url";	
-	private static final String PROPERTY_DRIVER = "driver";
-	private static final String PROPERTY_LOGIN = "login";
-	private static final String PROPERTY_PASSWD = "passwd";
-	
-	private DAOFactory daoFactory;
-	private Properties properties;
-	private ClassLoader classLoaderMock;
-	InputStream propertiesFile;
+		
+	DAOFactory daoFactory;
 
 	@Before
 	public void setUp() throws Exception {
-		//propertiesFile = cl.getResourceAsStream(PROPERTIES_FILE);
 		daoFactory = DAOFactory.getInstance();
 	}
 	
@@ -42,10 +26,4 @@ public class DAOFactoryTest {
 		DAOFactory daoFactory2 = DAOFactory.getInstance();
 		Assert.assertNotEquals(daoFactory, daoFactory2);
 	}
-	
-	@Test(expected = DAOConfException.class)
-	public void getingDAOFactoryInstanceWithNoPropertiesFile() {
-		
-	}
-
 }
